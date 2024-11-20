e6aff3336c9b7c0d48691e99f345edc08890aeea
Sanchjeev <sanchjeev.karikalan@imt-atlantique.net>
2024-11-20 16:00:43 +0100

    PlaceAdapteur

--- src/org/pneditor/petrinet/adapters/petrinet/PlaceAdapter.java
+++ src/org/pneditor/petrinet/adapters/petrinet/PlaceAdapter.java
@@ -4,38 +4,35 @@ import org.pneditor.petrinet.AbstractPlace;
 import org.pneditor.petrinet.models.petrinet.Place;
 
 public class PlaceAdapter extends AbstractPlace {
-	
+
 	Place adaptee;
-	
+
 	public PlaceAdapter(String label,Place place) {
 		super(label);
 		this.adaptee=place;
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void addToken() {
-		
-		
-		
+		adaptee.addToken(1);
 	}
 
 	@Override
 	public void removeToken() {
-		// TODO Auto-generated method stub
-		
+		adaptee.removeToken(1);
+
 	}
 
 	@Override
 	public int getTokens() {
-		// TODO Auto-generated method stub
-		return 0;
+		return adaptee.getToken();
 	}
 
 	@Override
 	public void setTokens(int tokens) {
-		// TODO Auto-generated method stub
-		
+		adaptee.removeToken(adaptee.getToken());
+		adaptee.addToken(tokens);
+
 	}
 
 }
