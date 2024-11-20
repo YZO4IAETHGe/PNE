afc2223c26451a39c4fd8125160a3d9f07d55bf9
Sanchjeev <sanchjeev.karikalan@imt-atlantique.net>
2024-11-20 15:41:03 +0100

    rajout du PetriNet

---
+++ src/org/pneditor/petrinet/adapters/initial/PlaceAdapter.java
@@ -0,0 +1,36 @@
+package org.pneditor.petrinet.adapters.initial;
+
+import org.pneditor.petrinet.AbstractPlace;
+
+public class PlaceAdapter extends AbstractPlace {
+
+	public PlaceAdapter(String label) {
+		super(label);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addToken() {
+		
+		
+	}
+
+	@Override
+	public void removeToken() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getTokens() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setTokens(int tokens) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
