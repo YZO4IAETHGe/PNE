58dfd037355495a98a8c7d53648a4e0b73d3279c
Sanchjeev <sanchjeev.karikalan@imt-atlantique.net>
2024-11-20 15:48:14 +0100

    dégitage de petrinet dans le model

---
+++ src/org/pneditor/petrinet/adapters/petrinet/PlaceAdapter.java
@@ -0,0 +1,38 @@
+package org.pneditor.petrinet.adapters.petrinet;
+
+import org.pneditor.petrinet.AbstractPlace;
+import org.pneditor.petrinet.models.petrinet.Place;
+
+public class PlaceAdapter extends AbstractPlace {
+
+	public PlaceAdapter(String label) {
+		super(label);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addToken() {
+		Place.class.add
+		
+		
+	}
+
+	@Override
+	public void removeToken() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getTokens() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setTokens(int tokens) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
