00b53db47be9274597dd652b74108a6e00316ee7
Sanchjeev <sanchjeev.karikalan@imt-atlantique.net>
2024-11-20 16:02:39 +0100

    TransitionAdapteur

---
+++ src/org/pneditor/petrinet/adapters/petrinet/TransitionAdapter.java
@@ -0,0 +1,14 @@
+package org.pneditor.petrinet.adapters.petrinet;
+
+import org.pneditor.petrinet.AbstractTransition;
+import org.pneditor.petrinet.models.petrinet.Transition;
+
+public class TransitionAdapter extends AbstractTransition {
+	
+	Transition adaptee;
+
+	public TransitionAdapter(String label) {
+		super(label);
+	}
+
+}
