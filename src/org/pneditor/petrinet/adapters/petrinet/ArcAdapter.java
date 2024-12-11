30ae6959fb0dd67ffe6d028ebc813b94850490b0
FriteSansSel <xujohan@outlook.fr>
2024-12-11 21:24:17 +0100

    ajout de commentaire pour les adapters

--- src/org/pneditor/petrinet/adapters/petrinet/ArcAdapter.java
+++ src/org/pneditor/petrinet/adapters/petrinet/ArcAdapter.java
@@ -1,8 +1,5 @@
 package org.pneditor.petrinet.adapters.petrinet;
 
-
-
-
 import org.pneditor.petrinet.AbstractArc;
 import org.pneditor.petrinet.AbstractNode;
 
@@ -13,17 +10,27 @@ import org.pneditor.petrinet.models.petrinet.ClearingArc;
 import org.pneditor.petrinet.models.petrinet.InhibitorArc;
 import org.pneditor.petrinet.models.petrinet.OutArc;
 
+//The ArcAdapter class acts as an adapter between the generic AbstractArc class 
+//and specific implementations of Arc in the PetriNet model.
 public class ArcAdapter extends AbstractArc {
 
+	// Instance of the specific Arc implementation being adapted.
 	Arc adaptee;
+	
+	// Associated transition and place adapters representing 
+    // the source and destination nodes of the arc.
 	TransitionAdapter transition;
 	PlaceAdapter place;
 
+	// Constructor to initialize the adapter with a specific Arc, TransitionAdapter, and PlaceAdapter.
 	public ArcAdapter(Arc arc,TransitionAdapter transition, PlaceAdapter place) {
 		adaptee=arc;
 		this.transition = transition;
 		this.place = place;
 	}
+	
+	// Returns the source node of the arc. 
+    // If the arc is an OutArc, the source is a transition; otherwise, it's a place.
 	@Override
 	public AbstractNode getSource() {
 		if (adaptee instanceof OutArc) {
@@ -34,6 +41,8 @@ public class ArcAdapter extends AbstractArc {
 		}
 	}
 
+	// Returns the destination node of the arc.
+    // If the arc is an OutArc, the destination is a place; otherwise, it's a transition.
 	@Override
 	public AbstractNode getDestination() {
 		if (adaptee instanceof OutArc) {
@@ -44,22 +53,26 @@ public class ArcAdapter extends AbstractArc {
 		}
 	}
 
-
+	// Determines if the arc is a reset arc (ClearingArc).
 	@Override
 	public boolean isReset() {
 		return adaptee instanceof ClearingArc;
 	}
 
+	// Determines if the arc is a regular arc (not ClearingArc or InhibitorArc).
 	@Override
 	public boolean isRegular() {
 		return !((adaptee instanceof ClearingArc) || (adaptee instanceof InhibitorArc));
 	}
 
+	// Determines if the arc is an inhibitory arc (InhibitorArc).
 	@Override
 	public boolean isInhibitory() {
 		return adaptee instanceof InhibitorArc;
 	}
 
+	// Retrieves the multiplicity (weight) of the arc.
+    // Throws a ResetArcMultiplicityException if the arc is a reset arc, as reset arcs do not have a multiplicity.
 	@Override
 	public int getMultiplicity() throws ResetArcMultiplicityException {
 		if (this.isReset()) {
@@ -69,6 +82,9 @@ public class ArcAdapter extends AbstractArc {
 		}
 	}
 
+	// Sets the multiplicity (weight) of the arc.
+    // Throws a ResetArcMultiplicityException if the arc is a reset arc.
+    // Ensures that the multiplicity is non-negative.
 	@Override
 	public void setMultiplicity(int multiplicity) throws ResetArcMultiplicityException {
 		if (this.isReset()) {
--- src/org/pneditor/petrinet/adapters/petrinet/PlaceAdapter.java
+++ src/org/pneditor/petrinet/adapters/petrinet/PlaceAdapter.java
@@ -3,31 +3,40 @@ package org.pneditor.petrinet.adapters.petrinet;
 import org.pneditor.petrinet.AbstractPlace;
 import org.pneditor.petrinet.models.petrinet.Place;
 
+//The PlaceAdapter class acts as an adapter between the generic AbstractPlace class
+//and the specific Place implementation in the PetriNet model.
 public class PlaceAdapter extends AbstractPlace {
 
+	// Instance of the specific Place implementation being adapted.
 	Place adaptee;
 
+	// Constructor to initialize the adapter with a label and a specific Place instance.
 	public PlaceAdapter(String label,Place place) {
 		super(label);
 		this.adaptee=place;
 	}
 
+	// Adds one token to the place by delegating the operation to the adapted Place instance.
 	@Override
 	public void addToken() {
 		adaptee.addToken(1);
 	}
 
+	// Removes one token from the place by delegating the operation to the adapted Place instance.
 	@Override
 	public void removeToken() {
 		adaptee.removeToken(1);
 
 	}
 
+	// Retrieves the current number of tokens in the place by delegating to the adapted Place instance.
 	@Override
 	public int getTokens() {
 		return adaptee.getToken();
 	}
 
+	// Sets the number of tokens in the place to the specified value.
+    // This is done by first removing all existing tokens and then adding the specified number of tokens.
 	@Override
 	public void setTokens(int tokens) {
 		adaptee.removeToken(adaptee.getToken());
